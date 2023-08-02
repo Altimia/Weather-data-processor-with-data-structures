9e3f672477d2507c5aa3645ffb3298cb273f50a5
YohanesFS <yohanesfelix@hotmail.com>
2023-08-02 20:37:32 +0800

    implement time and windlog

--- WindLog.h
+++ WindLog.h
@@ -14,17 +14,17 @@ private:
   double airTemp;
 
 public:
-  WindLog(Date d, Time t, double ws, double sr, double at) : date(d), time(t), windSpeed(ws), solarRad(sr), airTemp(at) {}
-  Date getDate() { return date; }
-  Time getTime() { return time; }
-  double getWindSpeed() { return windSpeed; }
-  double getSolarRad() { return solarRad; }
-  double getAirTemp() { return airTemp; }
-  void setDate(Date d) { date = d; }
-  void setTime(Time t) { time = t; }
-  void setWindSpeed(double ws) { windSpeed = ws; }
-  void setSolarRad(double sr) { solarRad = sr; }
-  void setAirTemp(double at) { airTemp = at; }
+  WindLog(Date d, Time t, double ws, double sr, double at);
+  Date getDate();
+  Time getTime();
+  double getWindSpeed();
+  double getSolarRad();
+  double getAirTemp();
+  void setDate(Date d);
+  void setTime(Time t);
+  void setWindSpeed(double ws);
+  void setSolarRad(double sr);
+  void setAirTemp(double at);
 };
 
 std::ostream& operator<<(std::ostream& os, const WindLog& wl);
